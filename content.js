ed01e9822b686a8e9d8097bcf110d1a88d6179bb
KPorus <designerfardin2021@gmail.com>
2024-02-21 13:02:47 +0600

    modified the file

--- content.js
+++ content.js
@@ -1,6 +1,4 @@
-// content.js
 function checkAndRedirect() {
-  // Define keywords related to adult content
   const adultKeywords = [
     "porn",
     "pron",
@@ -29,15 +27,11 @@ function checkAndRedirect() {
     "free-sex-vedio",
   ];
 
-  // Get the current URL
   const currentUrl = window.location.href.toLowerCase();
 
-  // Check if the current URL contains adult content keywords
   if (adultKeywords.some((keyword) => currentUrl.includes(keyword))) {
-    // Send a message to the background script
     chrome.runtime.sendMessage({ action: "redirect" });
   }
 }
 
-// Execute the function to check and redirect
 checkAndRedirect();
