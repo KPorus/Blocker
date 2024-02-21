5c18797106804a6aa77a4529b8679bf621953387
KPorus <designerfardin2021@gmail.com>
2024-02-21 14:32:51 +0600

    change the code structure

--- background.js
+++ background.js
@@ -1,9 +1,65 @@
+// background.js (service worker)
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "redirect") {
-    chrome.tabs.update(sender.tab.id, { url: "https://www.google.com" });
+  if (request.action === "checkAndRedirect") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length > 0) {
+        checkAndRedirect(tabs[0].id);
+      }
+    });
   }
 });
+
+
+function checkAndRedirect(tabId) {
+  const adultKeywords = [
+        "pron",
+        "jav",
+        "poorn",
+        "oxtube",
+        "xxx",
+        "nsfw",
+        "adult",
+        "fuck",
+        "pronhub",
+        "xnxx",
+        "xhamster",
+        "sexvid",
+        "fapmove",
+        "tube",
+        "sexindrag",
+        "sex",
+        "xzx",
+        "xlxx",
+        "xhofficial",
+        "pussyspace",
+        "letmejerk",
+        "xvedio",
+        "smutr",
+        "free-sex-vedio",
+        "adult",
+        "indian-adult-web-series",
+        "hot"
+  ];
+
+ chrome.tabs.get(tabId, (tab) => {
+   if (tab) {
+     const currentUrl = tab.url.toLowerCase();
+
+     if (adultKeywords.some((keyword) => currentUrl.includes(keyword))) {
+       chrome.tabs.sendMessage(tabId, { action: "redirect" });
+     }
+   }
+ });
+}
+
+chrome.webNavigation.onCompleted.addListener(
+  (details) => {
+    checkAndRedirect(details.tabId);
+  },
+  { url: [{ urlMatches: ".*" }] }
+);
--- content.js
+++ content.js
@@ -1,40 +1,59 @@
-function checkAndRedirect() {
-  const adultKeywords = [
-    "porn",
-    "pron",
-    "jav",
-    "poorn",
-    "oxtube",
-    "xxx",
-    "nsfw",
-    "adult",
-    "fuck",
-    "pronhub",
-    "xnxx",
-    "xhamster",
-    "sexvid",
-    "fapmove",
-    "tube",
-    "sexindrag",
-    "sex",
-    "xzx",
-    "xlxx",
-    "xhofficial",
-    "pussyspace",
-    "letmejerk",
-    "xvedio",
-    "smutr",
-    "free-sex-vedio",
-    "adult",
-    "indian-adult-web-series",
-    "hot"
-  ];
+// content.js
 
-  const currentUrl = window.location.href.toLowerCase();
+chrome.runtime.sendMessage({ action: "checkAndRedirect" });
 
-  if (adultKeywords.some((keyword) => currentUrl.includes(keyword))) {
-    chrome.runtime.sendMessage({ action: "redirect" });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "redirect") {
+    // Redirect the tab to a safe URL
+    window.location.href = "https://www.google.com";
   }
-}
+});
 
-checkAndRedirect();
+// const port = chrome.runtime.connect({ name: "content-script" });
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "redirect") {
+//     // Redirect the tab to a safe URL
+//     window.location.href = "https://www.google.com";
+//   }
+// });
+
+// checkAndRedirect();
+// function checkAndRedirect() {
+//   const adultKeywords = [
+//     "porn",
+//     "pron",
+//     "jav",
+//     "poorn",
+//     "oxtube",
+//     "xxx",
+//     "nsfw",
+//     "adult",
+//     "fuck",
+//     "pronhub",
+//     "xnxx",
+//     "xhamster",
+//     "sexvid",
+//     "fapmove",
+//     "tube",
+//     "sexindrag",
+//     "sex",
+//     "xzx",
+//     "xlxx",
+//     "xhofficial",
+//     "pussyspace",
+//     "letmejerk",
+//     "xvedio",
+//     "smutr",
+//     "free-sex-vedio",
+//     "adult",
+//     "indian-adult-web-series",
+//     "hot"
+//   ];
+
+//   const currentUrl = window.location.href.toLowerCase();
+
+//   if (adultKeywords.some((keyword) => currentUrl.includes(keyword))) {
+//     chrome.runtime.sendMessage({ action: "redirect" });
+//   }
+// }
